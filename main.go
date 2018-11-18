eb20f77e5132181fc880f12a2a384c6bc64c98ee
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2018-11-18 21:53:05 +0000

    Sort CORS

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/forecast", func(c *gin.Context) {
+	r.Use(CORSMiddleware())
+
+	r.GET("/forecast.json", func(c *gin.Context) {
 		lat := c.Query("lat")
 		lng := c.Query("lng")
 
@@ -24,4 +26,16 @@ func main() {
 		c.JSON(200, f)
 	})
 	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
 }
